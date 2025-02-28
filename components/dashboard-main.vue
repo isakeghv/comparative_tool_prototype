c5b7ad1940591bc80215d2c7593e46976fdece30
isakeh <150025599+isakeghv@users.noreply.github.com>
2025-02-28 15:56:57 +0100

    implemented the fundamental functionality for dashboard-filter, -main, -newstudy and studyblock. some updates to .md files and updated so dashboard has its own page file: not written in index.vue

---
+++ components/dashboard-main.vue
@@ -0,0 +1,20 @@
+<template>
+    <aside class="aside">
+        <div class="aside__container">
+            <Dashboard-newstudy @newstudy="(data) => console.log(data)" />
+            <Dashboard-filter @filter="(data) => console.log(data)" />
+        </div>
+    </aside>
+    <StudyBlock @select="(data) => console.log(data)" @edit="(data) => console.log(data)"
+        @delete="(data) => console.log(data)" @duplicate="(data) => console.log(data)"
+        @export="(data) => console.log(data)" id="555" title="Placeholder title" startDate="10/12/2024"/>
+
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+@import url('public/style/pages/dashboard/dashboard.css');
+</style>
--- components/studyBlock.vue
+++ components/studyBlock.vue
@@ -1,12 +1,61 @@
 <template>
-
+    <div class="card">
+        <img src="" alt="thumbnail of study" class="card__img">
+        <h3 class="card__title">{{ title }}</h3>
+        <p class="card__paragraph">Started: {{ startDate }}.</p>
+        <button class="card__select" aria-label="Open study" @click="studySelect(id)"></button>
+        <button class="card__button" aria-label="Open toolbar">
+            <div class="card__dot"></div>
+        </button>
+        <div class="popup">
+            <button class="popup__button" @click="studySelect(id)">Open</button>
+            <button class="popup__button" @click="studyEdit(id)">Edit</button>
+            <button class="popup__button" @click="studyDelete(id)">Delete</button>
+            <button class="popup__button" @click="studyDuplicate(id)">Duplicate</button>
+            <button class="popup__button" @click="studyExport(id, 'json')">Export JSON</button>
+            <button class="popup__button"  @click="studyExport(id, 'cvs')">Export CVS</button>
+        </div>
+    </div>
 </template>
 
 <script setup>
-//This component is for the study-block in the dashboard, which presents each study.
+const props = defineProps({
+    title: String,
+    startDate: String,
+    id: String,
+    status: String
+})
+
+//Defining events to emit
+const emit = defineEmits(['select', 'delete', 'edit', 'duplicate', 'export'])
+
+//emitting study id with "select" event
+const studySelect = (id) =>{
+    emit('select', id);
+}
+
+//emitting study id with "delete" event
+const studyDelete = (id) =>{
+    emit('delete', id);
+}
+
+//emitting study id with "edit" event
+const studyEdit = (id) =>{
+    emit('edit', id);
+}
+
+//emitting study id with "duplicate" event
+const studyDuplicate = (id) =>{
+    emit('duplicate', id);
+}
+
+//emitting study id and format with "export" event
+const studyExport = (id, format) =>{
+    emit('export', {id, format});
+}
 
 </script>
 
 <style scoped>
-
+@import url('public/style/components/studyBlock/studyBlock.css');
 </style>
