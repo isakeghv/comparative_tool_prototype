4ee8563c061ed6ec6788840c617e6a6c9fd5cc4b
isakeh <150025599+isakeghv@users.noreply.github.com>
2025-03-16 13:11:02 +0100

    fixed an invalid end-tag

--- components/study/study-main.vue
+++ components/study/study-main.vue
@@ -133,7 +133,6 @@
 
         {{ study }}
         </div>
-    </div>
     <!-- forward id/index of a specific question to e.g. update its response format -->
     <Study-aside :index="props.index" :id="props.id"/>
 </template>
