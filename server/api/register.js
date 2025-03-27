e1fbe4855a263a78e294c30ae49d44aee30d7c6f
Martin Aker <156419255+MartinKlevenAkersveen@users.noreply.github.com>
2025-03-27 14:43:18 +0100

    Validation and sanitization

--- server/api/register.js
+++ server/api/register.js
@@ -1,13 +1,16 @@
 import bcrypt from "bcryptjs";
 import { connDb } from "../utils/connDb.js";
 import { userCredential, userProfile } from '../models/user.js';
+import validator from "validator";
+import { readBody, setResponseStatus } from "h3";
 
-const checkEmail = async (email) => {
+
+const checkEmail = async (email, event) => {
 	const existingUser = await userCredential.findOne({ email });
 
 	if (existingUser) {
 		// setting response to 200 to confirm that email was found
-		setResponseStatus(200);
+		setResponseStatus(event, 200);
 		return { created: false, message: "User with this email already exists." };
 	}
 
@@ -38,31 +41,58 @@ const createUser = async (fname, lname, email, pwd) => {
 			newUserP.save()
 		]);
 
-		setResponseStatus(201);
+		setResponseStatus(event, 201);
 		
 		//return status and message that account has been created
 		return { created: true, message: "Account successfully created." };
 	} catch (err) {
 		console.log(err);
-		setResponseStatus(500);
+		setResponseStatus(event, 500);
 
 		//return false status and message that there was issue creating the account
 		return { created: false, message: "Issue occured while creating account." };
 	}
 }
 
-export default defineEventHandler(async (e) => {
+export default defineEventHandler(async (event) => {
 	// connect to db
 	await connDb();
-	const body = await readBody(e);
+	const body = await readBody(event);
 	const { firstname, lastname, email, password } = body;
 
+	//validation
+	if (!firstname || !lastname || !email || !password) {
+		setResponseStatus(event, 400)
+		return { created: false, message: "All fields are required"}
+	}
+
+	if (!validator.isEmail(email)){
+		setResponseStatus(event, 400)
+		return {created: false, message: "Invalid email format"}
+	}
+
+	if (!validator.isStrongPassword(password, {
+		minLength: 8,
+		minUppercase: 1,
+		minNumbers: 1
+	})) {
+		setResponseStatus(event, 400)
+		return { created:false, message: "Password must be at least 8 characters long and include, uppercase letter and a number"}
+	}
+
+
+	//Sanitize
+	const cleanFirstName = validator.escape(firstname.trim());
+	const cleanLastName = validator.escape(lastname.trim());
+	const cleanEmail = validator.normalizeEmail(email);
+	const cleanPassword = password.trim();
+
 	// if checkEmail returns a value, return and don't continue executing the rest of the code
-	const emailExist = await checkEmail(email);
+	const emailExist = await checkEmail(cleanEmail, event);
 	
 	if (emailExist) {		
 		return emailExist;
 	}	
 
-	return await createUser(firstname, lastname, email, password);
+	return await createUser(cleanFirstName, cleanLastName, cleanEmail, cleanPassword, event);
 });
