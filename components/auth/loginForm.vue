37160e66ef99290993309078ae45b7b828ade32b
Martin Aker <156419255+MartinKlevenAkersveen@users.noreply.github.com>
2025-05-07 22:37:54 +0200

    Fixed captcha issue #98
    It should be working correctly now. Worked in all my tests.

--- components/auth/loginForm.vue
+++ components/auth/loginForm.vue
@@ -97,6 +97,7 @@ const login = async () => {
 	if (!token) {
     loginStatus.value = "error";
     statusMsg.value = "CAPTCHA verification failed.";
+	window.turnstile?.reset();
     return;
   }
 	//calling backend function to login user
