43810153bd3beba28dd12092f2d1aeea149e29f6
isakeh <150025599+isakeghv@users.noreply.github.com>
2025-05-15 14:13:26 +0200

    fixed some introduction text at dashboard page

--- app/components/dashboard/dashboard-main.vue
+++ app/components/dashboard/dashboard-main.vue
@@ -14,14 +14,16 @@
 		<h2 class="main__headline font-h5 font-semi">{{ mainTitle }}</h2>
 		<div class="main__grid">
 			<!-- showing the create date instead of start date is temporary -->
-			<StudyCard v-for="study in studies" :key="study.id"
-				@select="(study) => emitSelectStudy(study)"
-				@edit="(study) => emitEditStudy(study)"
-				@delete="(study) => emitDeleteStudy(study)"
+			<StudyCard v-for="study in studies" :key="study.id" @select="(study) => emitSelectStudy(study)"
+				@edit="(study) => emitEditStudy(study)" @delete="(study) => emitDeleteStudy(study)"
 				@duplicate="(study) => studyDuplicate(study)" :study="study" :status="study.status" :filter="filter"
 				@export="(study) => console.log(study)" :id="study.id" :title="study.title"
 				:startDate="study.created" />
 		</div>
+		<p class="main__paragraph font-normal" v-if="noStudies">
+			You currently have no studies. <button class="main__button font-normal"
+				@click="createNewStudy()">Get started now!</button>
+		</p>
 	</main>
 </template>
 
@@ -44,6 +46,11 @@ studies.value = computedStudies.value;
 
 const emit = defineEmits(['newStudy', 'selectStudy', 'editStudy', 'deleteStudy']);
 
+const createNewStudy = () =>{
+	const studyID= crypto.randomUUID();
+	emitNewStudy(studyID);
+}
+
 // emit them again to reach `Dashboard` component that handles the showing of dashboard and study editor
 const emitNewStudy = (id) => emit('newStudy', id);
 const emitSelectStudy = (study) => emit('selectStudy', study);
@@ -57,24 +64,21 @@ const mainTitle = computed(() => {
 	return `Viewing ${filter.value}`;
 });
 
-//to return array of studies to display saved in a reactive variable
-// const studies = computed(() => {
-// 	return user.studies;
-// });
+const noStudies = computed(() => !studies.value || studies.value.length === 0)
 
 const studyDuplicate = (originalStudy) => {
-    const clone = JSON.parse(JSON.stringify(originalStudy));
-    clone.id = `${originalStudy.id}_copy_${Date.now().toString(36)}${Math.random().toString(36).slice(2, 6)}`;
+	const clone = JSON.parse(JSON.stringify(originalStudy));
+	clone.id = `${originalStudy.id}_copy_${Date.now().toString(36)}${Math.random().toString(36).slice(2, 6)}`;
 
-    const index = user.studies.findIndex(s => s.id === originalStudy.id);
-    if (index !== -1) {
-        user.studies.splice(index + 1, 0, clone);
-    } else {
-        user.studies.push(clone);
-    }
+	const index = user.studies.findIndex(s => s.id === originalStudy.id);
+	if (index !== -1) {
+		user.studies.splice(index + 1, 0, clone);
+	} else {
+		user.studies.push(clone);
+	}
 };
 
-const searchStudy = (query) =>{
+const searchStudy = (query) => {
 	filter.value = 'all';
 	const results = search(studies.value, query);
 
@@ -84,5 +88,5 @@ const searchStudy = (query) =>{
 </script>
 
 <style scoped>
-	@import url("public/style/pages/dashboard/dashboard.scss");
+@import url("public/style/pages/dashboard/dashboard.scss");
 </style>
